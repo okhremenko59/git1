a841e0062af3df1afd2e9b800c14ade45c10a5bc
okhremenko59 <okhremenko@bk.ru>
2022-08-24 17:05:56 +0500

    Add view Main and README

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SalesManager manager = new SalesManager(new int[]{1, 2, 3, 4, 5});
+        System.out.println(manager.max());
+    }
+}
