93b052dd021535306a6c59830d1a679a86a8e735
okhremenko59 <okhremenko@bk.ru>
2022-08-26 12:49:14 +0500

    add avg method

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,26 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+             if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    int avg;
+    public int avg() {
+        int sum=0;
+        int sMinMax=min()+max();
+        for (int i =0; i<sales.length; i++) {
+            sum+=sales[i];
+            }
+        avg=sum-sMinMax;
+        return avg;
+    }
+
 }
