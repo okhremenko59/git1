8591ba5c7d6b20b4ca75b637abd88209bec8e9ad
okhremenko59 <okhremenko@bk.ru>
2022-08-29 13:13:21 +0500

    reformat code and 'long avg' input method

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -18,21 +18,21 @@ public class SalesManager {
     public long min() {
         long min = sales[0];
         for (long sale : sales) {
-             if (sale < min) {
+            if (sale < min) {
                 min = sale;
             }
         }
         return min;
     }
 
-    long avg;
+
     public long avg() {
-        long sum=0;
-        long sMinMax=min()+max();
-        for (int i =0; i<sales.length; i++) {
-            sum+=sales[i];
-            }
-        avg=sum-sMinMax;
+        long sum = 0;
+        long sMinMax = min() + max();
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        long avg = sum - sMinMax;
         return avg;
     }
 
